6e099fbc853d7d8a1041a2f81af98363b32837ed
elephant <elephant@mbp.local>
2015-11-16 10:58:13 +0800

    Initial commit

---
+++ src/test/java/com/elephant/hadoop/WordCountTest.java
@@ -0,0 +1,38 @@
+package com.elephant.hadoop;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class WordCountTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public WordCountTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( WordCountTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
